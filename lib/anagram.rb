21788b869952cb5b86a101dd82f62ec6f1ea6404
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 18:12:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,6 +13,6 @@ end
 listen = Anagram.new("listen")
 listen.match(%w(enlists google inlets banana))
 
-if "asdf".split(" ").sort == "sdfa".split(" ").sort
+if "asdf".split(" ").sort == "asdf".split(" ").sort
   puts "hey"
 end

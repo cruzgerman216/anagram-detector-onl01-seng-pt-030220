70fe4f9a6a5fd5a709210c7ab8f6c3938c91a223
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 18:14:10 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,8 +13,6 @@ end
 listen = Anagram.new("listen")
 listen.match(%w(enlists google inlets banana))
 
-if "asdf".split(" ").sort.join("") == "fsda".split(" ").sort.join("")
+if "asdf".split("").sort.join("") == "fsda".split("").sort.join("")
   puts "hey"
 end
-puts "asdf".split("").sort.join("")
-puts "fsda".split("").sort.join("")

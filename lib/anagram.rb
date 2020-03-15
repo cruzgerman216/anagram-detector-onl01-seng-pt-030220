674385fa1124413486202e3c687b41d162f24cf8
German Cruz <cruzgerman216@yahoo.com>
2020-03-15 18:17:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -20,7 +20,3 @@ class Anagram
 end
 listen = Anagram.new("listen")
 puts listen.match(%w(enlists google inlets banana))
-
-if "listen".split("").sort.join("") == "inlets".split("").sort.join("")
-  puts "hey"
-end
